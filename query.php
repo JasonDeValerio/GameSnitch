ab1d43abd04545c0afb28e982ff3222ee2eee40b
JasonDeValerio <48882748+JasonDeValerio@users.noreply.github.com>
2019-08-26 14:48:16 -0400

    Reconfigure backend, remove unnecessary files, properly comment backend code
    Redid the query.php to work in a self contained fashion, no more script hopping to query, fill, and refresh results from the DB. Commented code in query.php for readability. Removed Extra PHP file previously used to fill DB

--- query.php
+++ query.php
@@ -26,30 +26,96 @@ else
     $pass = "";
     $dbName = "GamesDB";
 
-    $conn = mysqli_connect($serverName, $userName, $pass, "sys");
+    //Connect to DB
+    $conn = mysqli_connect($serverName, $userName, $pass, $dbName);
     mysqli_select_db($conn, $dbName);
     if(!$conn)
     {
         die("Connection failed: " . mysqli_connect_error());
     }
-    else{
+    else
+    {
+        //Create Search Query String
         $sqlInput = "SELECT * FROM games WHERE gameName LIKE '%$query%' LIMIT 5";
+        //Attempt Query
         $result = $conn->query($sqlInput);
+        
+        //If any matches are found
         if($result->num_rows > 0)
         {
             while($row=$result->fetch_assoc())
             {
                 $title = $row["gameName"];
+                $image = $row["imageURL"];
+                echo "<div><img src=$image><h4>$title</h4></div>";
             }
         }
+        //If no matches are found on the first pass
         else
         {
-            $title = "0 results";
+            $searchItem = sanitizeInput($_POST["search_bar"]);
+
+            //Make API call to attempt a match
+            $results = getGame($searchItem);
+            
+            //Base input query string
+            $inputBase = 'INSERT INTO games (gameName, gameDesc, imageURL, releaseDate, website) VALUES (';
+            
+            //For each item in api call results, assemble columns from data
+            foreach($results as $value)
+            {
+                $name = $value->name;
+                $summary = 'No summary';
+                if(property_exists($value, 'summary'))
+                {
+                    $summary = addslashes($value->summary);
+                }
+                $cover = 'No cover Art';
+                if(property_exists($value, 'cover'))
+                {
+                    $cover = addslashes($value->cover->url);
+                }
+                $release_date = 'Not Yet Released';
+                if(property_exists($value, 'first_release_date'))
+                {
+                    $release_date = date('m.d.y', $value->first_release_date);
+                }
+                $website = 'No Website';
+                if(property_exists($value, 'websites'))
+                {
+                    $website = addslashes($value->websites[0]->url);
+                }
+
+                //Complete query string with API call result
+                $query = "$inputBase" . "\"$name\", \"$summary\", \"$cover\", \"$release_date\", \"$website\");";
+                $conn->query($query);
+            }
+
+            //Attempt second pass at DB query
+            $sqlInput = "SELECT * FROM games WHERE gameName LIKE '%$query%' LIMIT 5";
+            $result = $conn->query($sqlInput);
+            
+            //If matches are found
+            if($result != null)
+            {
+                while($row=$result->fetch_assoc())
+                {
+                    $title = $row["gameName"];
+                    $image = $row["imageURL"];
+                    echo "<div><img src=$image><h4>$title</h4></div>";
+                }
+            echo "<script>document.getElementById('search_bar').value=\"" . sanitizeInput($_POST["search_bar"]) . "\";document.getElementById('search_form').submit();</script>";
+            }
+            //If DB is still lacking matches
+            else
+            {
+                echo "0 Results Found.";
+            }
         }
     }
-    echo $title;
 }
 
+//Format input for mysql query use
 function sanitizeInput($input)
 {
     $input = trim($input);
@@ -57,6 +123,50 @@ function sanitizeInput($input)
     $input = htmlspecialchars($input);
 
     return $input;
+}
+
+//API call to IGDB for games whose title matches input param
+function getGame($input)
+{
+    //replace spaces
+    $query = urlencode($input);
+    //create curl
+    $curl = curl_init();
+    //set url
+    $url = "https://api-v3.igdb.com/games/?fields=name,summary,cover.url,first_release_date,websites.url&search=$query";
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //set user agent
+    curl_setopt($curl, CURLOPT_USERAGENT, "Devotastic");
+    //set to post
+    curl_setopt($curl, CURLOPT_POST, false);
+
+    //Borrowed from online
+    // Fail the cURL request if response code = 400 (like 404 errors) 
+    curl_setopt($curl, CURLOPT_FAILONERROR, true);
+    // Wait for 10 seconds to connect, set 0 to wait indefinitely
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+    // Execute the cURL request for a maximum of 50 seconds
+    curl_setopt($curl, CURLOPT_TIMEOUT, 50);
+    // Do not check the SSL certificates
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); 
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    //set headers
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'user-key: b0d87f5b4f023c0d6ee4747fc28273c6',
+        'Accept: application/json',
+        'Content-Type: application/json',
+    ));
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+    //Execute
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure:" . curl_error($curl));}
+    curl_close($curl);
+
+    return json_decode($result);
 }
 ?></h1></div>
 		</div>
